9d2182ce830d60a317fa5d8e7d251f3adafedd26
Divyanshu Sharma <sharmadivyanshu9752@gmail.com>
2025-01-22 14:58:46 +0530

    change then cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const cookieParser = require('cookie-parser');
 const cors = require("cors");
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    //origin: "http://localhost:5173",
+    origin: "dev-tinder-pn2m7k3k4-divyanshu3181s-projects.vercel.app",
     credentials: true,
 }))
 app.use(express.json());

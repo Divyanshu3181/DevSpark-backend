38abc35dfe55e0e25153be76f3930f2a76259b6a
Divyanshu Sharma <sharmadivyanshu9752@gmail.com>
2025-01-22 14:01:11 +0530

    create .env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://namastedev:1234@cluster0.wuxyg6t.mongodb.net/devTinder"
+        process.env.DATABASE_URI
     );
 };
 
